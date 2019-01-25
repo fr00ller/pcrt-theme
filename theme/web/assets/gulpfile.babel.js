7881dfacc4d8af896876ae83f79b8183a88f9940
Marco Petrini <marco@bhima.eu>
2019-01-25 19:03:41 +0100

    Setting base

---
+++ theme/web/assets/gulpfile.babel.js
@@ -0,0 +1,66 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import babel from 'gulp-babel';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import cleanCSS from 'gulp-clean-css';
+import del from 'del';
+
+const paths = {
+  styles: {
+    src: 'css/config.scss',
+    dest: 'dist/'
+  },
+  scripts: {
+    src: ['src/*.js', 'node_modules/jquery/dist/jquery.min.js'],
+    dest: 'dist/'
+  },
+  watchStyles: {
+    src: 'css/*.scss',
+  }
+};
+
+/*
+ * For small tasks you can export arrow functions
+ */
+export const clean = () => del([ 'dist' ]);
+
+/*
+ * You can also declare named functions and export them as tasks
+ */
+export function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(concat('main.min.css'))
+    // pass in options to the stream
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+export function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+ /*
+  * You could even use `export as` to rename exported tasks
+  */
+function watchFiles() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.watchStyles.src, styles);
+}
+export { watchFiles as watch };
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts));
+/*
+ * Export a default task
+ */
+export default build;
