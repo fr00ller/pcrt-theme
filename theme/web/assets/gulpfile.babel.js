511da95d050e7f3a209f7fafa85f29c74230c71a
fr00ller <marco@bhima.eu>
2019-01-26 01:54:00 +0100

    Add widget Select2 Modify Gulp task to generate asset for select2

--- theme/web/assets/gulpfile.babel.js
+++ theme/web/assets/gulpfile.babel.js
@@ -5,20 +5,42 @@ import concat from 'gulp-concat';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
 import cleanCSS from 'gulp-clean-css';
+import merge from 'merge2';
+import request from 'request';
+import buffer from 'gulp-buffer';
+
 import del from 'del';
 
 const paths = {
   styles: {
-    src: 'css/config.scss',
-    dest: 'dist/'
+    main :{
+        src: 'css/main.scss',
+        dest: 'dist/'
+    },
+    select2 : {
+        src: 'css/select2.scss',
+        dest: '../../widgets/select2/assets/css/'
+    },
   },
   scripts: {
-    src: ['src/*.js', 'node_modules/jquery/dist/jquery.min.js'],
-    dest: 'dist/'
+    main:{
+      src: [
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/infinite-scroll/dist/infinite-scroll.pkgd.js',
+        'src/*.js'
+      ],
+      dest: 'dist/'
+    },
+    select2:{
+      src: 'node_modules/select2/dist/js/**/*',
+      dest: '../../widgets/select2/assets/js/'
+    }
+
   },
-  watchStyles: {
-    src: 'css/*.scss',
+  watchCSS: {
+    src: 'css/*.scss'
   }
+
 };
 
 /*
@@ -29,8 +51,8 @@ export const clean = () => del([ 'dist' ]);
 /*
  * You can also declare named functions and export them as tasks
  */
-export function styles() {
-  return gulp.src(paths.styles.src)
+export function cssmain() {
+  return gulp.src(paths.styles.main.src)
     .pipe(sass())
     .pipe(cleanCSS())
     .pipe(concat('main.min.css'))
@@ -39,27 +61,68 @@ export function styles() {
       basename: 'main',
       suffix: '.min'
     }))
-    .pipe(gulp.dest(paths.styles.dest));
+    .pipe(gulp.dest(paths.styles.main.dest));
+}
+
+export function cssselect2() {
+  return gulp.src(paths.styles.select2.src)
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(concat('main.min.css'))
+    // pass in options to the stream
+    .pipe(rename({
+      basename: 'select2',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.select2.dest))
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(rename({
+      basename: 'select2',
+      suffix: ''
+    }))
+    .pipe(gulp.dest(paths.styles.select2.dest))
+    ;
 }
 
 export function scripts() {
-  return gulp.src(paths.scripts.src, { sourcemaps: true })
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest));
+
+  return gulp.src(paths.scripts.main.src, { sourcemaps: true })
+  .pipe(buffer())
+  .pipe(concat('main.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest(paths.scripts.main.dest));
+
+
 }
+
+export function cloneSelect2js() {
+
+  return gulp.src(paths.scripts.select2.src).pipe(gulp.dest(paths.scripts.select2.dest));
+
+}
+
+export function select2js() {
+
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+  .pipe(buffer())
+  .pipe(concat('main.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest(paths.scripts.dest));
+
+}
+
 
  /*
   * You could even use `export as` to rename exported tasks
   */
 function watchFiles() {
   gulp.watch(paths.scripts.src, scripts);
-  gulp.watch(paths.watchStyles.src, styles);
+  gulp.watch(paths.watchCSS, cssmain );
+  gulp.watch(paths.watchCSS, cssselect2 );
 }
 export { watchFiles as watch };
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts));
+const build = gulp.series(clean, gulp.parallel(cssmain, cssselect2, scripts, cloneSelect2js));
 /*
  * Export a default task
  */
--- theme/web/assets/src/utility.js
+++ theme/web/assets/src/utility.js
@@ -1,6 +1,27 @@
-//export function Utility() {
-  function toggleSidebar(){
-    $('.pcrt-sidebar').toggleClass('pcrt-close')
-    $('.pcrt-wrapper').toggleClass('pcrt-close')
+function toggleSidebar(){
+  $('.pcrt-sidebar').toggleClass('pcrt-close');
+  $('.pcrt-wrapper').toggleClass('pcrt-close');
+}
+
+function infiniteScroll(_url){
+
+  $('.pcrt-wrapper-grid').hide();
+  $('.loader').show();
+  if(window.infScroll !== undefined){
+      window.infScroll.destroy();
   }
-//}
+  $('.pcrt-main-body').html('');
+  var elem = document.querySelector('.pcrt-main-body');
+  window.infScroll = new InfiniteScroll( elem, {
+    path: function() {
+        var page = this.pageIndex-1;
+        return _url+page;
+    },
+    append: '.row-item',
+    history: false,
+  });
+  window.infScroll.loadNextPage();
+  $('.loader').hide();
+  $('.pcrt-wrapper-grid').show();
+
+}
